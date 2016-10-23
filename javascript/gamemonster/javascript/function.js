71d2bbe3fa8e9207f038c3e96b060955512e2964
Vo Thanh Truc <vttvtn090194@gmail.com>
2016-10-24 00:44:45 +0700

    commit game monster

--- javascript/gamemonster/javascript/function.js
+++ javascript/gamemonster/javascript/function.js
@@ -5,8 +5,8 @@ var contextPlay = canvasPlay.getContext("2d");
 var framePlaySize = 500;
 var positionCenter = (framePlaySize - 120) / 2;
 
-
 // value default
+var stage = 1;
 var time = 20;
 var score = 0;
 var heart = 5;
@@ -17,8 +17,8 @@ var numBoom = 3;
 var buttonReset = false;
 sessionStorage.myVariable = 0;
 
-// imgBlood src
-var src = "../gamemonster/img/monster.gif";
+// img src
+var srcMonster = "../gamemonster/img/monster.gif";
 var srcBlood = "../gamemonster/img/blood.png";
 var srcNull = "";
 var srcHeart = "../gamemonster/img/heart.png";
@@ -32,7 +32,7 @@ var positionHeart = 75;
 var imgBlood = new Array();
 var imgBloodX = new Array();
 var imgBloodY = new Array();
-for (var i = 0; i <= 9; i++) {
+for (var i = 0; i < 9; i++) {
 	imgBlood[i] = new Image();
 	imgBlood[i].src = srcNull;
 	imgBloodX[i] = 0;
@@ -41,7 +41,10 @@ for (var i = 0; i <= 9; i++) {
 
 // list monster
 var imgMonster = new Array();
-var monster = new Array();
+for (var i = 0; i < 9; i++) {
+	imgMonster[i] = new Image();
+	imgMonster[i].src = srcMonster;
+}
 var monsterX = new Array();
 var monsterY = new Array();
 var speedX = new Array();
@@ -59,8 +62,8 @@ contextControl.fillText(score, 75, 25);
 contextControl.fillText("Heart: ", 10, 55);
 contextControl.fillText("Speed:", 10, 85);
 contextControl.fillText(speed, 80, 85);
-contextControl.fillText("High Score:", 350, 25);
-contextControl.fillText(sessionStorage.myVariable, 465, 25);
+contextControl.fillText("High Score:", 330, 25);
+contextControl.fillText(sessionStorage.myVariable, 445, 25);
 
 main();
 
@@ -70,26 +73,113 @@ function main() {
 	setInterval("play()", time);
 }
 
+function returnDefaultState() {
+	// monster top left
+	monsterX[0] = 0;
+	monsterY[0] = 0;
+
+	// monster top
+	monsterX[1] = positionCenter;
+	monsterY[1] = 0;
+
+	// monster top right
+	monsterX[2] = framePlaySize - 120;
+	monsterY[2] = 0;
+
+	// monster right
+	monsterX[3] = framePlaySize - 120;
+	monsterY[3] = positionCenter;
+
+	// monster bottom right
+	monsterX[4] = framePlaySize - 120;
+	monsterY[4] = framePlaySize - 120;
+
+	// monster bottom
+	monsterX[5] = positionCenter;
+	monsterY[5] = framePlaySize - 120;
+
+	// monster bottom left
+	monsterX[6] = 0;
+	monsterY[6] = framePlaySize - 120;
+
+	// monster left
+	monsterX[7] = 0;
+	monsterY[7] = positionCenter;
+
+	// monster center
+	monsterX[8] = positionCenter;
+	monsterY[8] = positionCenter;
+
+	// chang sum monster
+	numMonster += 9;
+}
+
+function returnDefaultSpeed() {
+	// speed top left
+	speedX[0] = 1;
+	speedY[0] = 1;
+
+	// speed top
+	speedX[1] = 0;
+	speedY[1] = 1;
+
+	// speed top right
+	speedX[2] = -1;
+	speedY[2] = 1;
+
+	// speed right
+	speedX[3] = -1;
+	speedY[3] = 0;
+
+	// speed bottom right
+	speedX[4] = -1;
+	speedY[4] = -1;
+
+	// speed bottom
+	speedX[5] = 0;
+	speedY[5] = -1;
+
+	// speed bottom left
+	speedX[6] = 1;
+	speedY[6] = -1;
+
+	// speed left
+	speedX[7] = 1;
+	speedY[7] = 0;
+
+	// speed center
+	speedX[8] = 2;
+	speedY[8] = 1;
+}
+
 function play() {
 	showHeart();
 	document.getElementById("num_boom").innerHTML = numBoom;
+	// click button reset
 	if (buttonReset == true) {
 		resetGame();
-		contextControl.clearRect(465, 0, 100, 50);
-		contextControl.fillText(sessionStorage.myVariable, 465, 25);
+		contextControl.clearRect(445, 0, 100, 50);
+		contextControl.fillText(sessionStorage.myVariable, 445, 25);
 		buttonReset = false;
 	} else {
+		// click button pause
 		if (buttonPause == false) {
 			document.getElementById("frame-play").setAttribute("onclick",
 					"catchMonster(event)");
 			if (heart > 0) {
 				if (numMonster == 0) {
-					startState();
-					increaseLevel();
+					returnDefaultState();
+					// when winning the monster is faster
+					if (stage > 1) {
+						increaseLevel();
+						// when stage 1 the monster's speed is nomarl
+					} else {
+						returnDefaultSpeed();
+					}
 				} else {
 					move();
 				}
-			} else {
+			} else { // when game over
 				contextPlay.clearRect(0, 0, framePlaySize, framePlaySize);
 				contextPlay.font = '50pt Calibri';
 				contextPlay.fillStyle = 'white';
@@ -98,7 +188,7 @@ function play() {
 				document.getElementById("frame-play").setAttribute("onclick",
 						"");
 			}
-		} else {
+		} else { // when click pause
 			contextPlay.font = '50pt Calibri';
 			contextPlay.fillStyle = 'white';
 			contextPlay.fillText("PAUSE", 170, 250);
@@ -116,15 +206,16 @@ function resetGame() {
 	if (score > sessionStorage.myVariable) {
 		sessionStorage.myVariable = score;
 	}
-	startState();
-	time = 20;
+	returnDefaultState();
+	returnDefaultSpeed();
+	stage = 1;
 	score = 0;
 	heart = 5;
 	numMonster = 0;
 	speed = -1;
 	buttonPause = false;
 	numBoom = 3;
-	showCore(score);
+	showScore(score);
 	showHeart();
 	increaseLevel();
 	for (var i = 0; i <= 9; i++) {
@@ -140,23 +231,28 @@ function clickBoom() {
 	if (numBoom > 0) {
 		numBoom--;
 		for (var i = 8; i >= 0; i--) {
-			imgBlood[i].src = srcBlood;
-			imgBloodX[i] = monsterX[i];
-			imgBloodY[i] = monsterY[i];
-			score += 10;
-			numMonster--;
-			speedX[i] = 0;
-			speedY[i] = 0;
-			monsterX[i] = 500;
-			monsterY[i] = 500;
+			// ignore the monster is dead
+			if (monsterX[i] == 500 || monsterY[i] == 500) {
+				continue;
+				// kill all monsters alive
+			} else {
+				imgBlood[i].src = srcBlood;
+				imgBloodX[i] = monsterX[i];
+				imgBloodY[i] = monsterY[i];
+				score += 10;
+				numMonster--;
+				monsterX[i] = 500;
+				monsterY[i] = 500;
+			}
 		}
 		// change score
-		showCore(score);
+		showScore(score);
+		stage++;
 		document.getElementById("num_boom").innerHTML = numBoom;
 	}
 }
 
-function showCore(score) {
+function showScore(score) {
 	contextControl.clearRect(75, 0, 150, 30);
 	contextControl.fillText(score, 75, 25);
 }
@@ -172,6 +268,9 @@ function showHeart() {
 
 // tang toc do
 function increaseLevel() {
+	speed++;
+	contextControl.clearRect(80, 60, 130, 130);
+	contextControl.fillText(speed, 80, 85);
 	for (var i = 0; i < 9; i++) {
 		if (speedX[i] == 0) {
 			speedX[i] = 0;
@@ -188,92 +287,11 @@ function increaseLevel() {
 			speedY[i]++;
 		}
 	}
-	speed++;
-	contextControl.clearRect(80, 60, 130, 130);
-	contextControl.fillText(speed, 80, 85);
-}
-
-// tro ve trang thai ban dau
-function startState() {
-	// monster top left
-	monsterX[0] = 0;
-	monsterY[0] = 0;
-	speedX[0] = 1;
-	speedY[0] = 1;
-	imgMonster[0] = new Image();
-	imgMonster[0].src = src;
-
-	// monster top
-	monsterX[1] = positionCenter;
-	monsterY[1] = 0;
-	speedX[1] = 0;
-	speedY[1] = 1;
-	imgMonster[1] = new Image();
-	imgMonster[1].src = src;
-
-	// monster top right
-	monsterX[2] = framePlaySize - 120;
-	monsterY[2] = 0;
-	speedX[2] = -1;
-	speedY[2] = 1;
-	imgMonster[2] = new Image();
-	imgMonster[2].src = src;
-
-	// monster right
-	monsterX[3] = framePlaySize - 120;
-	monsterY[3] = positionCenter;
-	speedX[3] = -1;
-	speedY[3] = 0;
-	imgMonster[3] = new Image();
-	imgMonster[3].src = src;
-
-	// monster bottom right
-	monsterX[4] = framePlaySize - 120;
-	monsterY[4] = framePlaySize - 120;
-	speedX[4] = -1;
-	speedY[4] = -1;
-	imgMonster[4] = new Image();
-	imgMonster[4].src = src;
-
-	// monster bottom
-	monsterX[5] = positionCenter;
-	monsterY[5] = framePlaySize - 120;
-	speedX[5] = 0;
-	speedY[5] = -1;
-	imgMonster[5] = new Image();
-	imgMonster[5].src = src;
-
-	// monster bottom left
-	monsterX[6] = 0;
-	monsterY[6] = framePlaySize - 120;
-	speedX[6] = 1;
-	speedY[6] = -1;
-	imgMonster[6] = new Image();
-	imgMonster[6].src = src;
-
-	// monster left
-	monsterX[7] = 0;
-	monsterY[7] = positionCenter;
-	speedX[7] = 1;
-	speedY[7] = 0;
-	imgMonster[7] = new Image();
-	imgMonster[7].src = src;
-
-	// monster center
-	monsterX[8] = positionCenter;
-	monsterY[8] = positionCenter;
-	speedX[8] = 2;
-	speedY[8] = 1;
-	imgMonster[8] = new Image();
-	imgMonster[8].src = src;
-
-	// chang sum monster
-	numMonster += 9;
 }
 
 function move() {
 	contextPlay.clearRect(0, 0, framePlaySize, framePlaySize);
-	// draw array imgBlood blood hide
+	// draw array img blood hide
 	for (var i = 0; i < 9; i++) {
 		contextPlay.drawImage(imgBlood[i], imgBloodX[i], imgBloodY[i]);
 	}
@@ -288,68 +306,36 @@ function move() {
 }
 
 function collision() {
-	// monster top left
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterY[0] >= this.framePlaySize - 120 || monsterY[0] < 0) {
-		speedX[0] = -speedX[0];
-		speedY[0] = -speedY[0];
+	for (var i = 0; i < 9; i++) {
+		// monster right or left
+		if ((i == 3) || (i == 7)) {
+			// touch the right or left wall monsters will change direction
+			if (monsterX[i] > this.framePlaySize - 120 || monsterX[i] < 0)
+				speedX[i] = -speedX[i];
+			// monster center
+		} else if (i == 8) {
+			// touch the right or left wall monsters will change direction
+			if (monsterX[i] > this.framePlaySize - 120 || monsterX[i] < 0)
+				speedX[i] = -speedX[i];
+			// touch the top or bottom wall monsters will change direction
+			if (monsterY[i] > this.framePlaySize - 120 || monsterY[i] < 0)
+				speedY[i] = -speedY[i];
+			// the remaining monsters
+		} else {
+			// touch the top or bottom wall monsters will change direction
+			if (monsterY[i] > this.framePlaySize - 120 || monsterY[i] < 0) {
+				speedX[i] = -speedX[i];
+				speedY[i] = -speedY[i];
+			}
+		}
 	}
-
-	// monster top
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterY[1] >= this.framePlaySize - 120 || monsterY[1] < 0)
-		speedY[1] = -speedY[1];
-
-	// monster top right
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterY[2] < 0 || monsterY[2] >= this.framePlaySize - 120) {
-		speedX[2] = -speedX[2];
-		speedY[2] = -speedY[2];
-	}
-
-	// monster right
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterX[3] >= this.framePlaySize - 120 || monsterX[3] < 0)
-		speedX[3] = -speedX[3];
-
-	// monster bottom right
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterY[4] < 0 || monsterY[4] > this.framePlaySize - 120) {
-		speedX[4] = -speedX[4];
-		speedY[4] = -speedY[4];
-	}
-
-	// monster bottom
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterY[5] >= this.framePlaySize - 120 || monsterY[5] < 0)
-		speedY[5] = -speedY[5];
-
-	// monster bottom left
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterY[6] < 0 || monsterY[6] > this.framePlaySize - 120) {
-		speedX[6] = -speedX[6];
-		speedY[6] = -speedY[6];
-	}
-
-	// monster left
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterX[7] >= this.framePlaySize - 120 || monsterX[7] < 0)
-		speedX[7] = -speedX[7];
-
-	// monster center
-	// cham vach ben phai hoac ben trai se doi huong
-	if (monsterX[8] >= this.framePlaySize - 120 || monsterX[8] < 0)
-		speedX[8] = -speedX[8];
-	// cham vach ben tren hoac ben duoi se doi huong
-	if (monsterY[8] >= this.framePlaySize - 120 || monsterY[8] < 0)
-		speedY[8] = -speedY[8];
 }
 
 function catchMonster(evt) {
 	var x = evt.clientX - canvasPlay.offsetLeft;
 	var y = evt.clientY - canvasPlay.offsetTop;
-	// click mouse monster center
-	for (var i = 9; i >= 0; i--) {
+	for (var i = 8; i >= 0; i--) {
+		// click position coincides with the position of monsters
 		if (x > monsterX[i] && x <= monsterX[i] + 120 && y > monsterY[i]
 				&& y <= monsterY[i] + 120) {
 			imgBlood[i].src = srcBlood;
@@ -358,16 +344,19 @@ function catchMonster(evt) {
 			score += 10;
 			numMonster--;
 			heart++;
-			speedX[i] = 0;
-			speedY[i] = 0;
 			monsterX[i] = 500;
 			monsterY[i] = 500;
 			break;
 		}
 	}
+	// kill all monster will level up
+	if (numMonster == 0) {
+		stage++;
+	}
+	// click wrong
 	heart--;
 	// change heart
 	showHeart();
 	// change score
-	showCore(score);
+	showScore(score);
 }
