4840da906a438873e0764e1207115b7894f6a43a
Vo Thanh Truc <vttvtn090194@gmail.com>
2016-10-29 17:24:58 +0700

    commit jquery

---
+++ jquery/formajax/jquery/function.js
@@ -0,0 +1,282 @@
+﻿$(document).ready(function() {
+// calender---------------------------------------------------------------------------------------------------
+	var check = true;
+	var today = new Date();
+	var nowYear = today.getFullYear();
+	var nowMonth = today.getMonth();
+	var nowDate = today.getDate();
+	var monthSelected = 0; // month seleted option
+	var yearSelected = 0; // year seleted option
+	
+	// show calender
+	$('#img_date').click(function() {
+		if (check == true) {
+			$('#calender').show();
+		} else {
+			$('#calender').hide();
+		}
+		check = !check;
+	});
+	
+	// display list option year
+	$('#list-year').append(function() {
+		var listYear = "";
+		for (var i = (nowYear - 300); i <= (nowYear + 100); i++) {
+			if (i == nowYear) {
+				listYear += '<option selected>'
+						+ i + '</option>';
+			} else {
+				listYear += '<option>' + i
+						+ '</option>';
+			}
+		}
+		return listYear;
+	});
+	
+	// display list option month
+	$('#list-month').append(function() {
+		var listMonth = "";
+		for (var i = 0; i <= 11; i++) {
+			if (i == nowMonth) {
+				listMonth += '<option selected>'
+						+ changeMonth(i) + '</option>';
+			} else {
+				listMonth += '<option>'
+						+ changeMonth(i) + '</option>';
+			}
+		}
+		return listMonth;
+	});
+
+	showDate();
+
+	function showDate() {
+		var idDate = "";
+		yearSelected = Number($('#list-year > option:selected').text());
+		monthSelected = $('#list-month > option:selected').index();
+		var dateCurrent = new Date(changeMonth(monthSelected)	+ " 1, " + (yearSelected));
+		var dayCurrent = dateCurrent.getDay();
+		var date = 1;
+		for (var i = 0; i < 42; i++) {
+			idDate = "#calender td:eq(" + i + ")";
+			// xac dinh ngay bat dau va ngay ket thuc
+			if ((i >= dayCurrent) && (date <= sumDateOfMonth(monthSelected,yearSelected))) {
+				// xac dinh ngay hien tai
+				if (date == nowDate && monthSelected == nowMonth && (yearSelected) == nowYear) {
+					$(idDate).attr('class', 'today');
+				} else {
+					$(idDate).attr('class', 'day');
+				}
+				$(idDate).text(date);
+				date++;
+			} else {
+				$(idDate).attr('class', 'not-day');
+				$(idDate).text("");
+			}
+		}
+	}
+
+	$('#list-month').click(function() { // choose month --> change date 
+		showDate();
+	});
+	$('#list-year').click(function() {  // choose year --> change date
+		showDate();
+	});
+
+	// click next year
+	$('#next_year').click(function() { 
+		try {
+			yearSelected = Number($('#list-year > option:selected').text());
+			if (yearSelected >= (nowYear + 100)) {
+				throw "number of year too big!"
+			} else {
+				// add attribute selected to option
+				// element of year next
+				$('#list-year > option:selected').prop("selected", false).next().prop("selected", true);
+				showDate();
+			}
+		} catch (err) {
+			alert(err);
+		}
+	});
+
+	// click previous year
+	$('#pre_year').click(function() {
+		yearSelected = Number($('#list-year > option:selected').text());
+		try {
+			if (yearSelected <= (nowYear - 300)) {
+				throw "number of year too small!";
+			} else {
+				// add attribute selected to
+				// option element of year next
+				$('#list-year > option:selected').prop("selected", false).prev().prop("selected",true);
+				showDate();
+			}
+		} catch (err) {
+			alert(err);
+		}
+	});
+
+	// click next month
+	$('#next_month').click(function() {
+						monthSelected = $('#list-month > option:selected').index();
+						yearSelected = Number($('#list-year > option:selected').text());
+						try {
+							// if december next january
+							if (monthSelected == 11) {
+								if (yearSelected < (nowYear + 100)) {
+									$('#list-month > option:selected').prop("selected",false);
+									$('#list-month > option:eq(1)').prev().prop("selected",true);
+									$('#list-year > option:selected').prop("selected",false).next().prop("selected",true);
+									showDate();
+								} else {
+									throw "number of year too big!"
+								}
+							} else {
+								$('#list-month > option:selected').prop("selected", false).next().prop("selected", true);
+								showDate();
+							}
+						} catch (err) {
+							alert(err);
+						}
+					});
+
+	// click previous month
+	$('#pre_month').click(function() {
+		monthSelected = $('#list-month > option:selected').index();
+		yearSelected = Number($('#list-year > option:selected').text());
+		try {
+			// if january previous december
+			if (monthSelected == 0) {
+				if (yearSelected > (nowYear - 300)) {
+					$('#list-month > option:selected').prop("selected",false);
+					$('#list-month > option:eq(10)').next().prop("selected",true);
+					$('#list-year > option:selected').prop("selected",false).prev().prop("selected",true);
+					showDate();
+				} else {
+					throw "number of year too small!"
+				}
+			} else {
+				$('#list-month > option:selected').prop("selected", false).prev().prop("selected",true);
+				showDate();
+			}
+		} catch (err) {
+			alert(err);
+		}
+	});
+
+	// input date
+	var dateChosen = "";
+	for (var i = 0; i < 42; i++) {
+		dateChosen = '#calender td:eq(' + i + ')';
+		chooseDate(dateChosen);
+	}
+
+	// click date add <input>
+	function chooseDate(dateChosen) {
+		$(dateChosen).click(function() {
+			if ($(dateChosen).text() != "") {
+				monthSelected = $('#list-month > option:selected').index();
+				yearSelected = Number($('#list-year > option:selected').text());
+				var dateSelected = $(dateChosen).text() + "/" + (monthSelected + 1) + "/" + yearSelected;
+				$('#in-date').val(dateSelected);
+			}
+		});
+	}
+
+	function checkLeadYear(year) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} else if (year % 4 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function sumDateOfMonth(month, year) {
+		switch (month) {
+		case 1:
+			if (checkLeadYear(year)) {
+				return 29;
+			} else {
+				return 28;
+			}
+		case 3:
+			return 30;
+		case 5:
+			return 30;
+		case 8:
+			return 30;
+		case 10:
+			return 30;
+		default:
+			return 31;
+		}
+	}
+
+	// change month num --> text
+	function changeMonth(num) {
+		switch (num) {
+		case 0:
+			return "January";
+		case 1:
+			return "February";
+		case 2:
+			return "March";
+		case 3:
+			return "April";
+		case 4:
+			return "May";
+		case 5:
+			return "June";
+		case 6:
+			return "July";
+		case 7:
+			return "August";
+		case 8:
+			return "September";
+		case 9:
+			return "October";
+		case 10:
+			return "November";
+		default:
+			return "December";
+		}
+	}
+
+// check user ---------------------------------------------------------------------------------------------------
+	$('#user-name').blur(function() {
+		var user = $('#user-name').val().length;
+		if (user <= 8) {
+			$('#error-user').text("Username length min 8 letter");
+		} else {
+			$('#error-user').text("");
+		}
+	});
+
+// check password------------------------------------------------------------------------------------------------
+	$('#password').blur(function() {
+		var password = $('#password').val().length;
+		if (password <= 8) {
+			$('#error-password').text("Password length min 8 letter");
+		} else {
+			$('#error-password').text("");
+		}
+	});
+
+// check email---------------------------------------------------------------------------------------------------
+	$('#email').blur(function() {
+		var email = $('#email').val();
+		if (email.search("@") < 1 || email.lastIndexOf(".") < email.search("@") + 2
+			|| email.lastIndexOf(".") + 2 > email.lenght) {
+			$('#error-email').text("Email wrong format");
+		} else {
+			$('#error-email').text("");
+		}
+	});
+})
---
+++ jquery/slider/jquery/function.js
@@ -0,0 +1,119 @@
+﻿$(document).ready(function() {
+	// list src of image
+	var listImg = [ 
+		"../slider/img/field2.jpg",
+		"../slider/img/fog.jpg",
+		"../slider/img/lake.jpg",
+		"../slider/img/lake_and_moutain.jpg",
+		"../slider/img/moutain.jpg",
+		"../slider/img/river.jpg",
+		"../slider/img/spring.jpg",
+		"../slider/img/sunshine.jpg",
+		"../slider/img/waterfall.jpg",
+		"../slider/img/winter.jpg" 
+	];
+	// list alt of image
+	var listNameOfImg = [ 
+		"field2", 
+		"fog", 
+		"lake",
+		"lake_and_moutain", 
+		"moutain", 
+		"river", 
+		"spring",
+		"sunshine", 
+		"waterfall", 
+		"winter" 
+	];
+	var imgCurrent = $("#image").attr("alt");
+	var btnNow = "";
+	var listButton = "";
+	// find image by alt
+	function search() {
+		var i = 0;
+		for (; i <= listNameOfImg.length - 1; i++) {
+			if (imgCurrent == listNameOfImg[i])
+				return i;
+		}
+		return -1;
+	}
+	// index of image current and list button index
+	$('#list_index').append(function() {
+		for (var i = 0; i < listNameOfImg.length; i++) {
+			if (i == search()) {
+				listButton += '<button id="btn_' + i + '" class="index index_select" type="button">' + i + '</button>';
+			} else {
+				listButton += '<button id="btn_' + i + '" class="index" type="button">' + i + '</button>';
+			}
+		}
+		return listButton;
+	});
+	// previous
+	$('#previous').click(function() {
+		try {
+			// if the first image
+			imgCurrent = $("#image").attr("alt");
+			btnNow = "#btn_" + search();
+			var btnPrevious = "";
+			if (search() == 0) {
+				btnPrevious = "#btn_" + (listImg.length - 1);
+				$('#image').attr('src', listImg[listImg.length - 1]);
+				$('#image').attr('alt', listNameOfImg[listNameOfImg.length - 1]);
+				$(btnPrevious).attr('class', 'index_select');
+				$(btnNow).removeClass('index_select');
+			} else if (search() == -1) {
+				throw "Image error!";
+			} else {
+				btnPrevious = "#btn_" + (search() - 1);
+				$('#image').attr('src',	listImg[search() - 1]);
+				$('#image').attr('alt',	listNameOfImg[search() - 1]);
+				$(btnPrevious).attr('class', 'index_select');
+				$(btnNow).removeClass('index_select');
+			}
+		} catch (err) {
+			window.alert(err);
+		}
+	});
+	// next
+	$('#next').click(function() {
+		try {
+			imgCurrent = $("#image").attr("alt");
+			btnNow = "#btn_" + search();
+			var btnNext = "";
+			// if the end image
+			if (search() == (listImg.length - 1)) {
+				btnNext = "#btn_0";
+				$('#image').attr('src', listImg[0]);
+				$('#image').attr('alt',	listNameOfImg[0]);
+				$(btnNext).attr('class', 'index_select');
+				$(btnNow).removeClass('index_select');
+			} else if (search() == -1) {
+				throw "Image error!";
+			} else {
+				btnNext = "#btn_" + (search() + 1);
+				$('#image').attr('src',	listImg[search() + 1]);
+				$('#image').attr('alt',	listNameOfImg[search() + 1]);
+				$(btnNext).attr('class', 'index_select');
+				$(btnNow).removeClass('index_select');
+			}
+		} catch (err) {
+			window.alert(err);
+		}
+	});
+	// function display image by index
+	function show_img_index(btnIndex, numIndex) {
+		$(btnIndex).click(function() {
+			imgCurrent = $("#image").attr("alt");
+			btnNow = "#btn_" + search();
+			$(btnNow).removeClass('index_select');
+			$('#image').attr('src', listImg[numIndex]);
+			$('#image').attr('alt', listNameOfImg[numIndex]);
+			$(btnIndex).attr('class', 'index_select');
+		});
+	}
+	// display image by index
+	for (var i = 0; i < listNameOfImg.length; i++) {
+		var btnIndex = "#btn_" + i;
+		show_img_index(btnIndex, i);
+	}
+})
